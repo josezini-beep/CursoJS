109baa4996401568ec5bf4050da2c803828786c4
josezini <jose.zini@plinenergia.com.br>
2026-09-04 16:29:35 -0300

    fiz o exercicio de naciionalidade que o guanabara tinha proposto na ultima aula

--- aula11/ex009.js
+++ aula11/ex009.js
@@ -1,6 +1,9 @@
 console.log ('o console executou corretamente')
 
-var vel = 60.8
+var vel = 50.8
+console.log(`a velocidade é ${vel}`)
 
-console.log(`a velocidade é${vel}`)
+    if (vel >= 60) { // é uma condição simples isso aq    
+        console.log(`vc foi multado`)
+}
 console.log('use cinto de segurança')
---
+++ aula11/ex010.js
@@ -0,0 +1,7 @@
+var pais = 'brasil'
+console.log (`vivendo no ${pais}`)
+if (pais=== 'Brasil') {
+    console.log('é brasileiro')
+} else {
+    console.log('vc é estrangeiro')
+}
