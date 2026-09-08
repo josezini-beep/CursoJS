d6f9bb89ba902ef3d64ca23f0abae1443950f445
josezini-beep <jose.zini@plinenergia.com.br>
2026-09-08 11:23:58 -0300

    fiz um caso com switch

---
+++ aula12/ex014.js
@@ -0,0 +1,36 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+/*
+esse ngc coloca os dias das semanas como numeros entt
+Domingo = 0
+Segunda = 1
+ETC
+*/
+
+console.log(diaSem)
+switch(diaSem){
+case 0:
+console.log('domingo')
+break
+case 1:
+console.log('segunda')
+break
+case 2:
+console.log('terca')
+break
+case 3:
+console.log('quarta')
+break
+case 4:
+console.log('quinta')
+break
+case 5:
+console.log('sexta')
+break
+case 6:
+console.log('sabado')
+break
+default:
+console.log('dia invalido')
+break
+}
--- aula12/node_984ddf47ada5b.tmp.js
+++
@@ -1,9 +0,0 @@
-var idade = 16
-if (idade < 16) {
-    console.log('Não vota')
-} else {
-    if (idade <18) {
-        console.log ('voto opcional')
-    } else if (idade>= 18)
-        console.log('voto obrigatório')
-} 
