2d5ef13cb2df2e8f03497bb61deaa7d069e58174
josezini-beep <jose.zini@plinenergia.com.br>
2026-09-08 08:51:04 -0300

    Sistema de hora

---
+++ aula12/ex013.js
@@ -0,0 +1,12 @@
+var hora = 3
+console.log (`agora são exatamente ${hora} hora`)
+    if (hora>=6 && hora<12) {
+    console.log(`bom dia`)
+    } else if (hora>=12 && hora <= 18) {
+        console.log('boa tarde')
+     }
+    else if (hora>18 && hora<=23) {
+        console.log('boa noite')
+    } else if (hora<6){
+        console.log('Boa madrugada')
+    }
