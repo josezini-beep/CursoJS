dd430d1a05fe4124fc998161885309ecd5c0abfd
josezini-beep <jose.zini@plinenergia.com.br>
2026-09-08 08:32:34 -0300

    adicionando condicional

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,10 +1,10 @@
-var idade = 16
+var idade = 35
 if (idade < 16) {
     console.log('Não vota')
 } else {
-    if (idade <18) {
+    if (idade <18 || idade>65) {
         console.log ('voto opcional')
-    } else if (idade>= 18) 
+    } else if (idade>= 18 ) 
         {
         console.log('voto obrigatório')
         }
