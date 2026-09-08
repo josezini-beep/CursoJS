fff9ad90a46aaaede55f1a2c58baa0a6cb19e50f
josezini-beep <jose.zini@plinenergia.com.br>
2026-09-08 08:24:20 -0300

    Testando condicionais compostas

---
+++ aula12/ex012.js
@@ -0,0 +1,9 @@
+var idade = 16
+if (idade < 16) {
+    console.log('Não vota')
+} else {
+    if (idade <18) {
+        console.log ('voto opcional')
+    } else if (idade>= 18)
+        console.log('voto obrigatório')
+} 
