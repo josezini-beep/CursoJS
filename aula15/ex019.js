40be09c9a9dc28d40f213932433ada2d3478710b
josezini-beep <jose.zini@plinenergia.com.br>
2026-09-09 10:30:03 -0300

    fazendo teste com vetores

---
+++ aula15/ex019.js
@@ -0,0 +1,8 @@
+let car=[1,2,3]
+car.push(1)
+car.sort()
+console.log(`[ ${car}] \nNosso vetor é ${car} e tem ${car.length} caracteres`)
+
+for (let i=0;i<car.length;i++){
+    console.log(car[i])
+}
