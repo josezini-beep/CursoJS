dc3346fe6697697a64e82581b7a838b09541e9b4
josezini-beep <jose.zini@plinenergia.com.br>
2026-09-09 08:33:04 -0300

    redimensionei a foto

--- aula12Ex2/script.js
+++ aula12Ex2/script.js
@@ -1,23 +1,29 @@
 function carregar(){
 var msg = window.document.getElementById('msg')
+var msg2 = window.document.getElementById('msg2')
 var img = window.document.getElementById('img')
 var data = new Date()
-var hora = data.getHours()
-msg.innerHTML=`agora são ${hora} horas`
+var hora = 4
+msg.innerHTML=`Agora são ${hora} horas`
 if (hora >=6 && hora <12){
     img.src="foto-manhã.jpg"
     document.body.style.background = 'yellow'
-
+    msg2.innerHTML = "Bom dia"
 } else if (hora >=12 && hora <18) {
     img.src="foto-tarde.jpg"
     document.body.style.background = 'orange'
-    
+    msg2.innerHTML = "Boa tarde"
 } else if (hora >=18 && hora <24) {
     img.src="foto-noite.jpg"
-    document.body.style.background = 'blue'
-} else if (hora >=0 && hora >6) {
+    document.body.style.background = '#024bb3'
+    msg2.innerHTML = "boa noite"
+} else if (hora >=0 && hora <6) {
     img.src="foto-madrugada.jpg"
-    document.body.style.background = '#191970'
-    
+    document.body.style.background = '#020241'
+    msg2.innerHTML = "Boa madrugada"
 }
+
+var img = window.document.getElementById('img')
+img.style.width = '250px'
+img.style.height = '250px'
 }
