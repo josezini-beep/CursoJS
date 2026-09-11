4fe0a3bf65a0fd854f4d93c1fc3fdbf7dc0fb5e9
josezini-beep <jose.zini@plinenergia.com.br>
2026-09-11 16:25:42 -0300

    n

--- ProjetosNelson/Planilhas2/script.js
+++ ProjetosNelson/Planilhas2/script.js
@@ -5,28 +5,27 @@ for (let i =1;i<=10;i++){
 }
 const textoCompleto = conteudo.join("\n");
 const linhas = textoCompleto.trim().split("\n");
-
 linhas.shift();
 const nomes = [];
 for (const linha of linhas) {
    const IdadeDados = linha.split(",");
    const idade = IdadeDados[4];
    if(idade>30){
-    const CpfDados = linha.split(",");
-    const cpf = CpfDados[5];
-      if(cpf!=undefined){
-          const trabalhoDados = linha.split(",");
-          const trabalho = trabalhoDados[3];
-          if(trabalho.includes("Gerente")||trabalho.includes("Diretor")||trabalho.includes("Coordenador")||trabalho.includes("Encarregado")||trabalho.includes("Líder")||trabalho.includes("Chefe")||trabalho.includes("Supervisor")) {
-            if (cpf[0]=="5"||cpf[0]=="7"||cpf[0]=="9"){
-              const NomeDados = linha.split(",");
-              const nome = NomeDados[0];
-              nomes.push(nome);
-            }
+      const CpfDados = linha.split(",");
+      const cpf = CpfDados[5];
+          if(cpf!=undefined){
+              const trabalhoDados = linha.split(",");
+              const trabalho = trabalhoDados[3];
+              if(trabalho.includes("Gerente")||trabalho.includes("Diretor")||trabalho.includes("Coordenador")||trabalho.includes("Encarregado")||trabalho.includes("Líder")||trabalho.includes("Chefe")||trabalho.includes("Supervisor")) {
+              if (cpf[0]=="5"||cpf[0]=="7"||cpf[0]=="9"){
+                const NomeDados = linha.split(",");
+                const nome = NomeDados[0];
+                 nomes.push(nome);
+              }
+           }
           }
-        }
-      }
     }
+  }
 const textoFinal = nomes.join("\n");
 
 fs.writeFileSync("nomes.txt", textoFinal, "utf8");
