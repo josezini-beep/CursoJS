f9f8fcbe4585b5507d62f4237cf477ca30c84295
josezini-beep <jose.zini@plinenergia.com.br>
2026-09-11 16:18:43 -0300

    fiz muita coisa

--- ProjetosNelson/Planilhas2/script.js
+++ ProjetosNelson/Planilhas2/script.js
@@ -1,55 +1,37 @@
 const fs = require("fs");
-
-const conteudo1 = fs.readFileSync("pessoas_01.csv", "utf8");
-
-
-const conteudo2 = fs.readFileSync("pessoas_02.csv", "utf8");
-
-
-const conteudo3 = fs.readFileSync("pessoas_03.csv", "utf8");
-
-
-const conteudo4 = fs.readFileSync("pessoas_04.csv", "utf8");
-
-
-const conteudo5 = fs.readFileSync("pessoas_05.csv", "utf8");
-
-
-const conteudo6 = fs.readFileSync("pessoas_06.csv", "utf8");
-
-
-const conteudo7 = fs.readFileSync("pessoas_07.csv", "utf8");
-
-
-const conteudo8 = fs.readFileSync("pessoas_08.csv", "utf8");
-
-
-const conteudo9 = fs.readFileSync("pessoas_09.csv", "utf8");
-
-
-const conteudo10 = fs.readFileSync("pessoas_10.csv", "utf8");
-
-
-const linhas = conteudo.trim().split("\n");
+const conteudo = [ ];
+for (let i =1;i<=10;i++){
+ conteudo[i] = fs.readFileSync(`pessoas_${i}.csv`, "utf8");
+}
+const textoCompleto = conteudo.join("\n");
+const linhas = textoCompleto.trim().split("\n");
 
 linhas.shift();
-
 const nomes = [];
-if 
 for (const linha of linhas) {
-
-  const dados = linha.split(",");
-
-  const nome = dados[0];
-
-  nomes.push(nome);
-}
-
+   const IdadeDados = linha.split(",");
+   const idade = IdadeDados[4];
+   if(idade>30){
+    const CpfDados = linha.split(",");
+    const cpf = CpfDados[5];
+      if(cpf!=undefined){
+          const trabalhoDados = linha.split(",");
+          const trabalho = trabalhoDados[3];
+          if(trabalho.includes("Gerente")||trabalho.includes("Diretor")||trabalho.includes("Coordenador")||trabalho.includes("Encarregado")||trabalho.includes("Líder")||trabalho.includes("Chefe")||trabalho.includes("Supervisor")) {
+            if (cpf[0]=="5"||cpf[0]=="7"||cpf[0]=="9"){
+              const NomeDados = linha.split(",");
+              const nome = NomeDados[0];
+              nomes.push(nome);
+            }
+          }
+        }
+      }
+    }
 const textoFinal = nomes.join("\n");
 
 fs.writeFileSync("nomes.txt", textoFinal, "utf8");
 
-console.log("Arquivo nomes.txt criado com sucesso!");
+console.log(textoFinal);
 
 //pegar todos ons nomes de todos os cpf's que comecem 5, 7, 9 que tenham um profissao ligada chefia de area com idade acima de 30 anos
 //pedir pra ia gerar um csv de 10 arquivos de 3k linhas com profissoes variadas
