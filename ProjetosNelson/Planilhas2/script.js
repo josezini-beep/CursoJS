f4ad262e7c2ca59de2523347fdc71d6b0440c9e4
josezini-beep <jose.zini@plinenergia.com.br>
2026-09-14 09:25:54 -0300

    terminando modificações

--- ProjetosNelson/Planilhas2/script.js
+++ ProjetosNelson/Planilhas2/script.js
@@ -6,27 +6,24 @@ for (let i =1;i<=10;i++){
 const textoCompleto = conteudo.join("\n");
 const linhas = textoCompleto.trim().split("\n");
 linhas.shift();
-const nomes = [];
+const resultado=[]
 for (const linha of linhas) {
-   const IdadeDados = linha.split(",");
-   const idade = IdadeDados[4];
+   const Dados = linha.split(",");
+   const cpf = Dados[5];
+   const idade = Dados[4];
+   const trabalho = Dados[3];
    if(idade>30){
-      const CpfDados = linha.split(",");
-      const cpf = CpfDados[5];
          if(cpf!=undefined){
-            const trabalhoDados = linha.split(",");
-            const trabalho = trabalhoDados[3];
             if(trabalho.includes("Gerente")||trabalho.includes("Diretor")||trabalho.includes("Coordenador")||trabalho.includes("Encarregado")||trabalho.includes("Líder")||trabalho.includes("Chefe")||trabalho.includes("Supervisor")) {
                if (cpf[0]=="5"||cpf[0]=="7"||cpf[0]=="9"){
                const NomeDados = linha.split(",");
-               const nome = NomeDados[0];
-               nomes.push(nome);
+               resultado.push(NomeDados.join(","));
               }
            }
         }
     }
   }
-const textoFinal = nomes.join("\n");
+const textoFinal = resultado.join("\n");
 
 fs.writeFileSync("nomes.txt", textoFinal, "utf8");
 
